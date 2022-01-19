7b0232ab570d0ed306014b07de613e56ef8cac24
kvelraj_concord <kvelraj@concord.net>
2022-01-19 08:51:47 +0530

    js

--- HTML/program.js
+++ HTML/program.js
@@ -7,3 +7,16 @@
 
 var score = 400;
 console.log(score);
+
+// String -> Any text value which is typed inside '' or ""
+
+var email = "karthick@gmail.com";
+console.log(email);
+
+// Boolean -> Either true or false
+
+var result_status = true;
+console.log(result_status);
+
+var future_value;
+console.log(future_value);
