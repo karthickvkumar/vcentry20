5d3c2004863127ac7bd6d8760eafa9138348d268
kvelraj_concord <kvelraj@concord.net>
2022-01-19 08:35:55 +0530

    js

---
+++ HTML/program.js
@@ -0,0 +1,9 @@
+// Number -> Any value numerical from Positive, Negative, Fractional, Decimal
+
+// Syntax To create a Variable (Temp Storage)
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var score = 400;
+console.log(score);
