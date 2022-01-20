df317f0e43dd4e227cf90f42d6b31b118d836c8d
kvelraj_concord <kvelraj@concord.net>
2022-01-20 08:50:50 +0530

    js

--- HTML/program.js
+++ HTML/program.js
@@ -20,3 +20,41 @@ console.log(result_status);
 
 var future_value;
 console.log(future_value);
+
+// Object -> It is collection of Key and Values
+
+// var variable_name = {
+//   key1 : value,
+//   key2 : value
+// }
+
+// key -> Similar to variable_name -> it should contains a-z, A-Z, 0-9, _, $
+// value -> Number, String, Boolean, Object, Array, Function
+
+var studentInfo = {
+  first_name : "Karthick",
+  last_name : "kumar",
+  father_name : "Xyz",
+  age : 27,
+  status : true
+}
+
+console.log(studentInfo);
+
+// Object -> 4 operation = read value, insert new value, edit exisiting value, delete existing value. 
+
+// To Read a Value -> object_variable_name.key;
+
+console.log(studentInfo.first_name);
+console.log(studentInfo.age);
+
+// To Insert a new Value -> object_variable_name.newKey = value;
+
+studentInfo.blood_group = "A +ve";
+
+// To Edit/Updated an exisiting value -> object_variable_name.existing_key = new_value;
+
+studentInfo.father_name = "Velraj";
+
+// To delete a existing Record -> delete object_variable_name.key
+delete studentInfo.status;
