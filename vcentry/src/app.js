0d37beede34ceae1e1adf47bfbf17e4c4aaeb009
kvelraj_concord <kvelraj@concord.net>
2022-02-08 08:23:55 +0530

    js

--- vcentry/src/app.js
+++ vcentry/src/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 
+import "./css/style.css";
+
 class App extends Component{
   render(){
     return(
       <div>
-        <h1>Welcome to React App</h1>
-        <h1>This is message is comming from App js</h1>
+        <h1 className="heading">Welcome to React App</h1>
+        <h1 className="sub-heading">This is message is comming from App js</h1>
       </div>
     )
   }
