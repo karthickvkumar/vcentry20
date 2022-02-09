5c18918775de3cb1071df7669e8f1e6978320f51
kvelraj_concord <kvelraj@concord.net>
2022-02-09 08:34:11 +0530

    js

--- vcentry/src/app.js
+++ vcentry/src/app.js
@@ -7,8 +7,6 @@ class App extends Component{
   render(){
     return(
       <div>
-        <h1 className="heading">Welcome to React App</h1>
-        <h1 className="sub-heading">This is message is comming from App js</h1>
         <DisplayMessage></DisplayMessage>
       </div>
     )
--- vcentry/src/components/display-message.js
+++ vcentry/src/components/display-message.js
@@ -2,10 +2,16 @@ import React, {Component} from "react";
 
 class DisplayMessage extends Component {
 
+  display(){
+    var message = "Welcome to Display Message method";
+    console.log(message);
+  }
+
   render(){
     return(
       <div>
         <h1>Hi, this is display message</h1>
+        <button onClick={() => this.display() }>Print Message</button>
       </div>
     )
   }
