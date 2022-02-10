aba95082508a39373c5e391d060c6ea202d05277
kvelraj_concord <kvelraj@concord.net>
2022-02-10 08:32:53 +0530

    event

--- vcentry/src/components/display-message.js
+++ vcentry/src/components/display-message.js
@@ -3,8 +3,18 @@ import React, {Component} from "react";
 class DisplayMessage extends Component {
 
   display(){
-    var message = "Welcome to Display Message method";
+    let message = "Welcome to Display Message method";
     console.log(message);
+  }
+
+  //On Change - Type 1
+  readInputBox(event){
+    console.log(event.target.value)
+  }
+
+  //On Change - Type 2 (Property method)
+  readInputPropertyMethod = (event) => {
+    console.log(event.target.value)
   }
 
   render(){
@@ -12,6 +22,15 @@ class DisplayMessage extends Component {
       <div>
         <h1>Hi, this is display message</h1>
         <button onClick={() => this.display() }>Print Message</button>
+        <br></br>
+        <label>Enter your UserName :</label>
+        {/* on Change - Type 1 */}
+        <input className="inputBox" type="text" placeholder="Enter you username.." 
+        onChange={this.readInputBox.bind(this)}/>
+        <br></br>
+        <label>Enter your Password :</label>
+        {/* on Change - Type 2 */}
+        <input className="inputBox" type="password" placeholder="Enter your Password.." onChange={this.readInputPropertyMethod}/>
       </div>
     )
   }
