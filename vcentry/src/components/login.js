d05b5170dda36d30d484d8c088572475cc72a168
kvelraj_concord <kvelraj@concord.net>
2022-02-16 08:18:03 +0530

    react

--- vcentry/src/components/login.js
+++ vcentry/src/components/login.js
@@ -8,22 +8,26 @@ class LoginPage extends Component{
     super(props);
     this.state = {
       username : '',
+      email : '',
       password : ''
     }
   }
 
   onHandleInput = (event) => {
     // console.log(event.target.name, event.target.value)
-    if(event.target.name == "User Name"){
-      this.setState({
-        username : event.target.value
-      })
-    }
-    else if(event.target.name == "Password"){
-      this.setState({
-        password : event.target.value
-      })
-    }
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+    // if(event.target.name == "User Name"){
+    //   this.setState({
+    //     username : event.target.value
+    //   })
+    // }
+    // else if(event.target.name == "Password"){
+    //   this.setState({
+    //     password : event.target.value
+    //   })
+    // }
   }
 
   onLogin(){
@@ -36,11 +40,15 @@ class LoginPage extends Component{
         <h1>Welcome, Login Page</h1>
         <div className="block-space">
           <label className="label-message">Please enter your Username :</label>
-          <input className="input-box" type="text" placeholder="Enter your Username.." onChange={this.onHandleInput} name="User Name"/>
+          <input className="input-box" type="text" placeholder="Enter your Username.." onChange={this.onHandleInput} name="username"/>
+        </div>
+        <div className="block-space">
+          <label className="label-message">Please enter your Email :</label>
+          <input className="input-box" type="text" placeholder="Enter your Email.." onChange={this.onHandleInput} name="email"/>
         </div>
         <div className="block-space">
           <label className="label-message">Please enter your Password :</label>
-          <input className="input-box" type="password" placeholder="Enter your Password.." onChange={this.onHandleInput} name="Password"/>
+          <input className="input-box" type="password" placeholder="Enter your Password.." onChange={this.onHandleInput} name="password"/>
         </div>
         <button className="button" onClick={() => this.onLogin()}>Login</button>
       </div>
