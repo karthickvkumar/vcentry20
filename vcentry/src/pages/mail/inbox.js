c493d8f8dba8a94423ae0ff8626f9c6833d6d416
kvelraj_concord <kvelraj@concord.net>
2022-03-03 08:54:49 +0530

    react

--- vcentry/src/pages/mail/inbox.js
+++ vcentry/src/pages/mail/inbox.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class InboxPage extends Component{
+
+  onLoadUser(){
+    let apiURL = "https://reqres.in/api/users?page=2";
+
+    axios.get(apiURL)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render(){
     return(
       <div>
         <h1>This is Inbox Page</h1>
+        <button onClick={() => this.onLoadUser()}>Load User Information</button>
       </div>
     )
   }
