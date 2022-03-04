a5d59d5137542faa850e219de4bda8ed9e6d296c
kvelraj_concord <kvelraj@concord.net>
2022-03-04 08:52:24 +0530

    react

--- vcentry/src/pages/mail/inbox.js
+++ vcentry/src/pages/mail/inbox.js
@@ -3,12 +3,28 @@ import axios from "axios";
 
 class InboxPage extends Component{
 
-  onLoadUser(){
-    let apiURL = "https://reqres.in/api/users?page=2";
+  constructor(props){
+    super(props);
+    this.state = {
+      userList: [],
+      isLoading: false
+    }
+  }
+
+  onLoadUser(page){
+    let apiURL = "https://reqres.in/api/users?page=" + page;
+    this.setState({
+      isLoading : true
+    })
 
     axios.get(apiURL)
       .then((response) => {
-        console.log(response)
+        let result = response.data;
+        console.log(result);
+        this.setState({
+          userList : result.data, 
+          isLoading : false
+        })
       })
       .catch((error) => {
         console.log(error)
@@ -16,10 +32,56 @@ class InboxPage extends Component{
   }
 
   render(){
+   let users = this.state.userList.map((value, index) => {
+    return(
+      <tr key={index}>
+        <td>{value.id}</td>
+        <td>{value.first_name}</td>
+        <td>{value.last_name}</td>
+        <td>{value.email}</td>
+        <td>
+          <img src={value.avatar} className="round-image"/>
+        </td>
+      </tr>
+    )
+   });
+
     return(
       <div>
         <h1>This is Inbox Page</h1>
-        <button onClick={() => this.onLoadUser()}>Load User Information</button>
+        <button onClick={() => this.onLoadUser(1)}>Load User Information</button>
+
+        {this.state.isLoading ? 
+            <img src={require("../../images/loader.gif")}/>
+            :
+            <table id="customers">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email ID</th>
+              <th>Profile Picture</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.userList.length === 0 ? 
+              <tr>
+                <td colSpan="5">No Record Found</td>
+              </tr>
+              :
+              users
+            }
+          </tbody>
+        </table>
+      }
+
+        <div>
+          <button className="btn btn-primay" onClick={() => this.onLoadUser(1)}>1</button>
+          <button className="btn btn-primay" onClick={() => this.onLoadUser(2)}>2</button>
+          <button className="btn btn-primay" onClick={() => this.onLoadUser(3)}>3</button>
+        </div>
       </div>
     )
   }
