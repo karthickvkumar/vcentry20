ceff2324bccb710fd60329939e1e2f683880f355
kvelraj_concord <kvelraj@concord.net>
2022-03-07 08:43:38 +0530

    react

--- vcentry/src/pages/mail/trash.js
+++ vcentry/src/pages/mail/trash.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class TrashPage extends Component{
+
+  onDelete(){
+    let apiURL = "https://reqres.in/api/users/2";
+
+    axios.delete(apiURL)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render(){
     return(
       <div>
         <h1>This is Trash Page</h1>
+        <button onClick={() => this.onDelete()}>Delete API</button>
       </div>
     )
   }
