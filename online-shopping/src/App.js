e9cbaaf846c8b1af0b5b0da024228928fae2e45c
kvelraj_concord <kvelraj@concord.net>
2022-03-14 09:00:40 +0530

    online project

---
+++ online-shopping/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import HomePage from "./pages/home";
+import ShopPage from "./pages/shop";
+import SingleProductPage from "./pages/single-product";
+import CartPage from "./pages/cart";
+import CheckoutPage from "./pages/checkout";
+
+import "./css/bootstrap.min.css";
+import "./css/font-awesome.min.css";
+import "./css/style.css";
+import "./css/responsive.css";
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="products" element={<ShopPage />}></Route>
+        <Route path="single-product" element={<SingleProductPage />}></Route>
+        <Route path="cart" element={<CartPage />}></Route>
+        <Route path="checkout" element={<CheckoutPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ online-shopping/src/pages/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HeaderComponent from "../components/header";
+import FooterComponent from "../components/footer";
+
+const HomePage = () => {
+  return(
+    <div>
+      <HeaderComponent></HeaderComponent>
+        <h1>sdf</h1>
+        <h1>sdf</h1>
+        <h1>sdf</h1>
+        <h1>sdf</h1>
+      <FooterComponent></FooterComponent>
+    </div>
+  )
+}
+
+export default HomePage;
