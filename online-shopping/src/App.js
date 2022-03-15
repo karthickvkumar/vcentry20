cede7a349720393fbfd74852ea095a816ddb6009
kvelraj_concord <kvelraj@concord.net>
2022-03-15 08:59:05 +0530

    project

--- online-shopping/src/App.js
+++ online-shopping/src/App.js
@@ -6,8 +6,10 @@ import ShopPage from "./pages/shop";
 import SingleProductPage from "./pages/single-product";
 import CartPage from "./pages/cart";
 import CheckoutPage from "./pages/checkout";
+import HeaderComponent from "./components/header";
 
 import "./css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/font-awesome.min.css";
 import "./css/style.css";
 import "./css/responsive.css";
@@ -15,6 +17,7 @@ import "./css/responsive.css";
 const App = () => {
   return(
     <BrowserRouter>
+      <HeaderComponent></HeaderComponent>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="products" element={<ShopPage />}></Route>
--- online-shopping/src/pages/shop.js
+++ online-shopping/src/pages/shop.js
@@ -1,9 +1,62 @@
 import React from "react";
+import ProductComponent from "../components/product";
 
 const ShopPage = () => {
   return(
     <div>
-      <h1>Shop Page</h1>
+      <div class="product-big-title-area">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="product-bit-title text-center">
+                          <h2>Smartphones</h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <div class="single-product-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+              
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                  <ProductComponent></ProductComponent>
+                </div>
+
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
